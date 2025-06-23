a4e9d7d3fc390d28e0fac3930cad09e77a9a12e4
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-06-23 16:16:47 +0530

    Delete data_lifecycle/ingestion/currated zone directory

--- data_lifecycle/ingestion/currated zone/cssi_agrwn_actv_ready_mix_lvl.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  *
-FROM
-  `bcs-csw-core-np.core_base_v.cssi_agrwn_actv_ready_mix_lvl`
-WHERE
-  action_type <> 'D'
--- data_lifecycle/ingestion/currated zone/cssi_agrwn_cultv_area.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  *
-FROM
-  `bcs-csw-core-np.core_base_v.cssi_agrwn_cultv_area`
-WHERE
-  action_type <> 'D'
