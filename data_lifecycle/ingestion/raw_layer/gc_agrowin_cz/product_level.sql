08f932759a67cb4d8e801026fb89f8efb1ed3149
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-22 15:12:51 +0530

    Update product_level.sql

---
+++ data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/product_level.sql
@@ -0,0 +1,24 @@
+#Advanced analytics - Step 24
+
+SELECT
+  year,
+  country,
+  indication,
+  crop_main_group,
+  crop_group,
+  crop,
+  area_grown_000_ha,
+  area_treated_000_ha,
+  area_tractor_000_ha,
+  area_product,
+  volume_product_000_kg_li,
+  value_user_000_eur,
+  value_distributor_000_eur,
+  exchange_rate,
+  local_currency_code,
+  local_currency,
+  value_user_000_local,
+  value_distributor_000_local,
+  "Product" as level,
+  product as dimension,
+FROM bcs-edf-ingestion.gc_agrowin_cz.product_left  
