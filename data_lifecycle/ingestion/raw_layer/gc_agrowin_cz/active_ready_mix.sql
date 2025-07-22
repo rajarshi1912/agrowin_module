1588dfb866d4e79e4c3983d1cc9f5af2d75dd03a
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-22 15:12:22 +0530

    Update active_ready_mix.sql

---
+++ data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/active_ready_mix.sql
@@ -0,0 +1,24 @@
+#Advanced analytics - Step 25
+
+SELECT
+  yr_nbr as year,
+  cntry_nm as country,
+  indctn_txt as indication,
+  crop_main_grp_cd as crop_main_group,
+  crop_grp_cd as crop_group,
+  crop_cd as crop,
+  actv_ready_mix_txt as active_ready_mix,
+  #area_grwn_000_ha as area_grown_000_ha, Step 28 to comment this field
+  area_treat_000_ha as area_treated_000_ha,
+  area_trctr_000_ha as area_tractor_000_ha,
+  area_prod_val as area_product,
+  vol_prod_000_kg_ltr_val as volume_product_000_kg_li,
+  usr_000_eur_val as value_user_000_eur,
+  distrb_000_eur_val as value_distributor_000_eur,
+  exchg_rt_val as exchange_rate,
+  lcl_crncy_cd as local_currency_code,
+  lcl_crncy_txt as local_currency,
+  usr_000_lcl_val as value_user_000_local,
+  distrb_000_lcl_val as value_distributor_000_local
+FROM
+  `bcs-edf-ingestion.gc_agrowin_cz.cssi_agrwn_actv_ready_mix_lvl`
