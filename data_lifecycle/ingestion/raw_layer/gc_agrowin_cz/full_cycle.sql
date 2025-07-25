6622b54961001e4ffb6b3daff5a170b6fb0d55fe
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-25 12:36:35 +0530

    Update full_cycle.sql

--- data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/full_cycle.sql
+++ data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/full_cycle.sql
@@ -31,4 +31,4 @@ SELECT
   usr_000_lcl_val as value_user_000_local,
   distrb_000_lcl_val as value_distributor_000_local
 FROM
-  `bcs-edf-ingestion.gc_agrowin_cz.cssi_agrwn_full_cycle`
+  `bcs-edf-ingestion-np.gc_agrowin_cz.cssi_agrwn_full_cycle`
