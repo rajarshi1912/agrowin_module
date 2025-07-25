cc0c3de7a569999efc0392112c4ca86fbb6c8ca2
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-25 12:36:19 +0530

    Update complete_data.sql

--- data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/complete_data.sql
+++ data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/complete_data.sql
@@ -1,6 +1,6 @@
 #Total Final Flow - Step 8
 
-SELECT * FROM `bcs-edf-ingestion.gc_agrowin_cz.comparable_data`
+SELECT * FROM `bcs-edf-ingestion-np.gc_agrowin_cz.comparable_data`
 UNION ALL
-SELECT * FROM `bcs-edf-ingestion.gc_agrowin_cz.all_data`
+SELECT * FROM `bcs-edf-ingestion-np.gc_agrowin_cz.all_data`
 ;
