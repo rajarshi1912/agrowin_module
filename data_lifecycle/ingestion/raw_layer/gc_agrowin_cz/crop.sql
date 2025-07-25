4272b0ec01a40d6907495cae98c53d27642c4780
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-25 12:36:23 +0530

    Update crop.sql

--- data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/crop.sql
+++ data_lifecycle/ingestion/raw_layer/gc_agrowin_cz/crop.sql
@@ -20,4 +20,4 @@ SELECT
   usr_000_lcl_val as value_user_000_local,
   distrb_000_lcl_val as value_distributor_000_local
 FROM
-  `bcs-edf-ingestion.gc_agrowin_cz.cssi_agrwn_crop_lvl`;
+  `bcs-edf-ingestion-np.gc_agrowin_cz.cssi_agrwn_crop_lvl`;
