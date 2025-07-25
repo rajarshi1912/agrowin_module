914fe923d77096984df3fc59422d744ea352367d
rajarshi1912 <rajarshi.chakraborty.ext@bayer.com>
2025-07-25 13:39:58 +0530

    Update blackbox_data_input_pivotted_v.sql

---
+++ data_lifecycle/ingestion/raw_layer/m_blackbox_cz/blackbox_data_input_pivotted_v.sql
@@ -0,0 +1,65 @@
+With Bayer_Pivotted as(
+SELECT * FROM `bcs-edf-ingestion-np.m_blackbox_cz.blackbox_data_input_v`
+UNPIVOT
+(
+  Bayer
+  FOR Year in (
+  Bayer_2018 AS "2018",
+  Bayer_2019 AS "2019",
+  Bayer_2020 AS "2020",
+  Bayer_2021 AS "2021",
+  Bayer_2022 AS "2022",
+  Bayer_2023 AS "2023",
+  Bayer_2024 AS "2024",
+  Bayer_2025 AS "2025",
+  Bayer_2026 AS "2026",
+  Bayer_2027 AS "2027")
+)
+),
+Blackbox_Pivotted as(
+SELECT * FROM `bcs-edf-ingestion-np.m_blackbox_cz.blackbox_data_input_v`
+UNPIVOT
+(
+  Blackbox
+  FOR Year in (
+  Blackbox_2018 AS "2018",
+  Blackbox_2019 AS "2019",
+  Blackbox_2020 AS "2020",
+  Blackbox_2021 AS "2021",
+  Blackbox_2022 AS "2022",
+  Blackbox_2023 AS "2023",
+  Blackbox_2024 AS "2024",
+  Blackbox_2025 AS "2025",
+  Blackbox_2026 AS "2026",
+  Blackbox_2027 AS "2027")
+)
+),
+Comments_Pivotted as(
+SELECT * FROM `bcs-edf-ingestion-np.m_blackbox_cz.blackbox_data_input_v`
+UNPIVOT
+(
+  Comments
+  FOR Year in (
+  Comments_2018 AS "2018",
+  Comments_2019 AS "2019",
+  Comments_2020 AS "2020",
+  Comments_2021 AS "2021",
+  Comments_2022 AS "2022",
+  Comments_2023 AS "2023",
+  Comments_2024 AS "2024",
+  Comments_2025 AS "2025",
+  Comments_2026 AS "2026",
+  Comments_2027 AS "2027")
+)
+)
+SELECT Bayer_Pivotted.Quarter,Bayer_Pivotted.Indication,Bayer_Pivotted.Local_Currency_code,Bayer_Pivotted.Country,Bayer_Pivotted.Year,Bayer_Pivotted.Bayer,Blackbox_Pivotted.Blackbox,Comments_Pivotted.Comments
+FROM Bayer_Pivotted LEFT JOIN Blackbox_Pivotted
+ON (Bayer_Pivotted.Year=Blackbox_Pivotted.Year
+AND Bayer_Pivotted.Quarter=Blackbox_Pivotted.Quarter
+AND Bayer_Pivotted.Indication=Blackbox_Pivotted.Indication
+AND Bayer_Pivotted.Country=Blackbox_Pivotted.Country)
+LEFT JOIN Comments_Pivotted
+ON (Bayer_Pivotted.Year=Comments_Pivotted.Year
+AND Bayer_Pivotted.Quarter=Comments_Pivotted.Quarter
+AND Bayer_Pivotted.Indication=Comments_Pivotted.Indication
+AND Bayer_Pivotted.Country=Comments_Pivotted.Country)
